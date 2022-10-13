df6eb96fb397ff977797cab6cddba1cca8b62bc1
hiroyone <hiroki.yonetani@zalando.de>
2022-10-13 10:06:14 +0200

    Loggin implementation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use std::{time::Duration, thread};
-
-use clap::Parser;
-use anyhow::{Context, Result};
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
-}
+use log::{info, warn};
 
 fn main() {
-    let pb = indicatif::ProgressBar::new(100);
-    for i in 0..100 {
-        thread::sleep(Duration::from_secs(2));
-        pb.println(format!("[+] finished #{}", i));
-        pb.inc(1);
-    }
-    pb.finish_with_message("done");
+    env_logger::init();
+    info!("starting up");
+    warn!("oops, nothing implemented!");
 }
-
-
-
